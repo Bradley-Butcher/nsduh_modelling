53afe8e8f7f9b587811e2f6dccf9c27a1a26b14f
Brad Butcher <bradbutcher@Brads-MBP.lan>
2022-12-28 18:50:48 +0000

    made functional rather than dataframes

--- cj_pipeline/neulaw/assignment_preprocessing.py
+++ cj_pipeline/neulaw/assignment_preprocessing.py
@@ -7,19 +7,21 @@ from cj_pipeline.config import logger
 
 base_path = Path(__file__).parents[2] / 'data'
 
-def get_offence_counts(start_year: int, window: int):
+
+def init_offence_counting(start_year: int, window: int):
+    logger.info("Preparing Offence Counting...")
     harrod_county = load(base_path / 'neulaw')
     # cut off the years before the start year
     harrod_county[harrod_county["calc.year"] >= start_year]
     max_year = harrod_county["calc.year"].max()
     # add year category according to window
-    start_windows = range(start_year, max_year - window + 1)
-    end_windows = range(start_year + window, max_year + 1)
-    logger.info("Starting Offence Counting")
-    for start_window, end_window in zip(start_windows, end_windows):
-        logger.info(f"Counting Offences from {start_window} to {end_window}")
-        count_df = count_year_range(harrod_county, start_window, end_window)
-        count_df.to_csv(base_path / 'processed' / f'offence_counts_{start_window}_{end_window}.csv')
+    def get_offence_counts(year: int):
+        if year > max_year:
+            raise ValueError(f"Year {year} is greater than max year {max_year}")
+        logger.info(f"Counting Offences from {year} to {year + window}")
+        count_df = count_year_range(harrod_county, year, year + window)
+        return count_df
+    return get_offence_counts
 
 
 def count_year_range(df: pd.DataFrame, start_year: int, end_year:int):
@@ -27,10 +29,22 @@ def count_year_range(df: pd.DataFrame, start_year: int, end_year:int):
     year_df = df[(df['calc.year'] >= start_year) & (df['calc.year'] < end_year)]
     year_df = year_df.groupby(groups).agg({'offense_category': 'count'})
     year_df = year_df.unstack(level=-1)
+    year_df = year_df.droplevel(level=0, axis=1)
     year_df = year_df.fillna(0)
     year_df["year_range"] = f"{start_year}-{end_year}"
+    year_df = year_df.reset_index().rename(columns={df.index.name:'index'})
+    year_df = year_df.rename(columns={'offense_category': 'index'})
+    # convert to integer
+    year_df["aggravated assault"] = year_df["aggravated assault"].astype(int)
+    year_df["robbery"] = year_df["robbery"].astype(int)
+    year_df["sex offense"] = year_df['sex offense'].astype(int)
+    year_df["simple assault"] = year_df["simple assault"].astype(int)
+    year_df["dui"] = year_df["dui"].astype(int)
+    year_df["property"] = year_df["property"].astype(int)
     return year_df
 
 
 if __name__ == '__main__':
-    get_offence_counts(2000, 5)
+    offense_counts = init_offence_counting(start_year=2000, window=3)
+    first_df = offense_counts(year=2000)
+    second_df = offense_counts(year=2001)
--- cj_pipeline/synthetic_assignment.py
+++ cj_pipeline/synthetic_assignment.py
@@ -6,3 +6,4 @@
 # For each year, if person was 18 or above, how many offenses of each type?
 
 # Assign unobserved offenses, based on their criminal activity
+
