82ea02501e1fbc1bd718c93f7932b435ea2133d7
Jiri Hron <jiri.m.hron@gmail.com>
2023-01-01 17:03:59 +0100

    fix nsduh age preprocessing

--- cj_pipeline/nsduh/preprocessing.py
+++ cj_pipeline/nsduh/preprocessing.py
@@ -23,18 +23,22 @@ def process_drug(df, drug_col):
 
 def process_catag6(df):
     df['CATAG6'] = df['CATAG6'].astype("int")
-    d = {
-        1: "12-17",
-        2: "18-25",
-        3: "26-34",
-        4: "35-49",
-        5: "50-64",
-        6: "65+"
-    }
-    df['CATAG6'] = df['CATAG6'].map(d)
+    # d = {  # TODO: see if commenting out breaks things
+    #     1: "12-17",
+    #     2: "18-25",
+    #     3: "26-34",
+    #     4: "35-49",
+    #     5: "50-64",
+    #     6: "65+"
+    # }
+    # df['CATAG6'] = df['CATAG6'].map(d)
     df = df.dropna(subset=['CATAG6'], axis=0)
     return df
 
+def process_catag7(df):
+    df['CATAG7'] = df['CATAG7'].astype("int")
+    df = df.dropna(subset=['CATAG7'], axis=0)
+    return df
 
 def process_newrace2(df):
     df['NEWRACE2'] = df['NEWRACE2'].astype("int")
@@ -93,6 +97,7 @@ variable_pp = {
     "IRHERFY": partial(process_drug, drug_col="IRHERFY"),
     "IRCRKFY": partial(process_drug, drug_col="IRCRKFY"),
     "CATAG6": process_catag6,
+    "CATAG7": process_catag7,
     "NEWRACE2": process_newrace2,
     # "EDUHIGHCAT": process_eduhighcat,
     "IRSEX": process_irsex,
@@ -111,7 +116,7 @@ variable_names = {
     "IRCOCFY": "Cocaine Past Year",
     "IRHERFY": "Heroin Past Year",
     "IRCRKFY": "Crack Past Year",
-    "CATAG6": "Age",
+    # "CATAG6": "Age",
     "NEWRACE2": "Race",
     "EDUHIGHCAT": "Education",
     "IRSEX": "Sex",
@@ -123,11 +128,28 @@ def get_variables():
     return list(variable_pp.keys())
 
 
+def add_age(df):
+  def _process(row):
+    if row['CATAG7'] in (1, 2, 3):
+      return '< 18'
+    if row['CATAG7'] in (4, 5, 6):
+      return '18-30'
+    if row['CATAG6'] in (4, 5, 6):
+      return '> 30'
+    return None
+
+  df['Age'] = df.apply(_process, axis=1)
+  df = df.drop(columns=['CATAG6', 'CATAG7'])
+
+  return df
+
 def preprocess(df: pd.DataFrame):
     for variable in get_variables():
         df = variable_pp[variable](df)
+    df = add_age(df)
     df = total_drug_use(df)
     df = df.rename(columns=variable_names)
+
     counts = df.groupby(["Race", "Age", "Sex"]).size().to_frame("count").reset_index()
     df = df.groupby(["Race", "Age", "Sex"]).agg(
         {
