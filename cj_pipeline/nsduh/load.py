b548ebcace2fb29bbb85db06c8040eb8a2f7a5b6
Jiri Hron <jiri.m.hron@gmail.com>
2023-01-10 12:26:23 +0000

    update data loading to 1992-2000

--- cj_pipeline/nsduh/load.py
+++ cj_pipeline/nsduh/load.py
@@ -3,22 +3,33 @@ from pathlib import Path
 from cj_pipeline.nsduh.preprocess import get_variables
 
 data_path = Path(__file__).parents[2] / 'data' / 'nsduh'
-years = [2013, 2014, 2015, 2016, 2017, 2018, 2019]
+# years = [2013, 2014, 2015, 2016, 2017, 2018, 2019]
+years = list(range(1992, 2020))  # TODO: uncomment !!!
+stata_years = [1999, 2000, 2001]
 
 
-def read_txt(year: int, max_rows: int) -> pd.DataFrame:
-    year_data_path = data_path / f'NSDUH_{year}_Tab.txt'
-    kwargs = {}
-    if max_rows > 0:
-        kwargs['nrows'] = max_rows
-    df = pd.read_csv(
-      year_data_path, sep='\t',
-      usecols=lambda c: c in get_variables(), **kwargs)
+def read_nsduh(year: int, max_rows: int) -> pd.DataFrame:
+    if year in stata_years:
+      year_data_path = data_path / f'NHSDA-{year}-DS0001-data-stata.dta'
+      if max_rows > 0:
+        reader = pd.read_stata(year_data_path, chunksize=max_rows)
+        df = next(reader)  # take just the first max_rows
+      else:
+        df = pd.read_stata(year_data_path)
+    else:
+      year_data_path = data_path / f'NSDUH_{year}_Tab.txt'
+      kwargs = {'nrows': max_rows} if max_rows > 0 else {}
+      df = pd.read_csv(
+        year_data_path, sep='\t',
+        usecols=lambda c: c in get_variables(),
+        **kwargs
+      )
+
     # df["csv_year"] = year  # TODO: why renamed?
-    df['YEAR'] = year  # TODO: temp reverted to match preprocess.py
+    df['YEAR'] = year  # TODO: temp reverted to match `preprocess.py`
     return df
 
 
 def load_nsduh(max_rows: int = -1) -> pd.DataFrame:
-    df = pd.concat([read_txt(year, max_rows) for year in years])
+    df = pd.concat([read_nsduh(year, max_rows) for year in years])
     return df
