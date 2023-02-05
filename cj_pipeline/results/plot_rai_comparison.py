8d9362663ef22d658fb2ea8e54de68ea726858f2
Jiri Hron <jiri.m.hron@gmail.com>
2023-02-05 16:30:47 +0000

    skip recomputing observed in plot_rai when no offset

--- cj_pipeline/results/plot_rai_comparison.py
+++ cj_pipeline/results/plot_rai_comparison.py
@@ -7,14 +7,14 @@ from cj_pipeline.results.compare_ates import aggregate
 from cj_pipeline.counterfactual_matching import average_treatment_effect
 
 
-def _load_data(ignore_cols=None):
+def _load_data(use_offset, ignore_cols=None):
   synth = aggregate(drop_constant_cols=True, ignore_cols=ignore_cols)
   if 'synth' in synth.columns and not all(synth['synth']):
     observed = synth[~synth['synth']]
     synth = synth[synth['synth']]
   else:
     synth['synth'] = True
-    observed = _load_observed()
+    observed = _load_observed() if use_offset else pd.DataFrame(columns=synth.columns)
   df = pd.concat([observed, synth], ignore_index=True)
 
   def _melt(id_vars, suffix, value_name):
@@ -29,9 +29,8 @@ def _load_data(ignore_cols=None):
     )
     return ans
 
-  id_vars = df.columns[df.nunique() > 1]
+  id_vars = df.columns[df.nunique() > 1].union({'synth'})
   id_vars = [c for c in id_vars if not c[:c.find('_')] in SCORES]
-  # id_vars = ['lam', 'omega', 'synth']
   df = pd.merge(
     _melt(id_vars, suffix='_mean', value_name='mean'),
     _melt(id_vars, suffix='_sem', value_name='sem'),
@@ -72,7 +71,7 @@ def plot_rais(df, gap=0.1, width=0.2, use_offset=False, exclude=None):
   sns.set_style('whitegrid')
   sns.set(font_scale=1.25)
 
-  synth, observed = df[df.synth], df[~df.synth]
+  synth, observed = df[df.synth], df[~df.synth] if use_offset else None
   for score in SCORES:
     idx = synth.score == score
     offset = observed[observed.score == score]['mean'].iloc[0] if use_offset else 0
@@ -96,7 +95,7 @@ def plot_rais(df, gap=0.1, width=0.2, use_offset=False, exclude=None):
 
   grid.fig.supxlabel(r'$\lambda$')
   grid.set_axis_labels(
-    x_var='', y_var='% point score change' if use_offset else 'Estimated treatment')
+    x_var='', y_var='% point score change' if use_offset else 'Estimated effect')
   grid.set_titles(row_template='{row_name}', col_template='{col_name}')
   grid.add_legend(title=r'$\omega$')
 
@@ -105,6 +104,7 @@ def plot_rais(df, gap=0.1, width=0.2, use_offset=False, exclude=None):
 
 
 if __name__ == '__main__':
-  ignore_cols = ['commit', 'crime_bins']
-  df = _load_data(ignore_cols=ignore_cols)
-  plot_rais(df, exclude=['fta'], use_offset=False)
+  use_offset = False
+  ignore_cols = ['commit']
+  df = _load_data(use_offset=use_offset, ignore_cols=ignore_cols)
+  plot_rais(df, exclude=['fta'], use_offset=use_offset)
